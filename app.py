f927b0ebd3b4d6f534cb5252f70e296641e9f0d2
qmoriceau <quentin.moriceau@ipilyon.net>
2020-11-14 17:07:09 +0100

    correction front

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL #For MySQL
+import MySQLdb.cursors #For MySQL
+import re
 
 app = Flask(__name__)
 
+#connection details for mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'StreamingSite'
+
+#initializing MySQL
+mysql = MySQL(app)
 
 @app.route('/')
 def accueil():
     return render_template('accueil.html')
+
+
+@app.route('/')
+def login():
+    return render_template()
