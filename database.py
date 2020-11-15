89944ffc27291f8b3f01e28076a453a401cff9fd
Florine Michel <florinemichel@orange.fr>
2020-11-15 14:16:48 +0100

    add values in database.py & def manga

--- database.py
+++ database.py
@@ -55,6 +55,14 @@ class Manga(Base):
 
 def __init__(self):
     self.title = title
+    self.alt_title = alt_title
+    self.category = category
+    self.description = description
+    self.release_date = release_date
+    self.ending_date = ending_date
+    self.studio = studio
+    self.nbr_volume = nbr_volume
+    self.nbr_chap = nbr_chap
 
 
 # create table
@@ -66,6 +74,8 @@ Base.metadata.create_all(engine)
 session = Session()"""
 
 engine.execute("INSERT INTO Users(username, password) VALUE (\"reuh\", \"reuh\")")
+engine.execute("INSERT INTO language(name) VALUES ('Arabic'), ('Bengali'), ('Bulgarian'), ('Burmese'), ('Catalan'), ('Chinese(Simp)'), ('Chinese(Hong-Kong)'), ('Czech'), ('Danish'), ('Dutch'), ('English'), ('Filipino'), ('Finnish'), ('French'), ('German'), ('Greek'), ('Hebrew'), ('Hindi'), ('Hungarian'), ('Indonesian'), ('Italian'), ('Japanese'), ('Korean'), ('Lithuanian'), ('Malay'), ('Mongolian'), ('Norwegian'), ('Other'), ('Persian'), ('Polish'), ('Portuguese(BR)'), ('Portuguese(PT)'), ('Romanian'), ('Russian'), ('Serbo-Russian'), ('Spanish(ES)'), ('Spanish(LATAM)'), ('Swedish'), ('Thai'), ('Turkish'), ('Ukrainian'), ('Vietnamese');")
+engine.execute("INSERT INTO mangas(title, alt_title, category, description, release_date, ending_date, studio, nbr_volume, nbr_chap, id_language) VALUES ('ワンピース', 'One Piece', 'Shonen', 'Gloire, fortune et puissance, c\'est ce que possédait Gold Roger, le tout puissant roi des pirates, avant de mourir sur l\'échafaud. Mais ses dernières paroles ont éveillées bien des convoitises, et lança la fabuleuse ère de la piraterie, chacun voulant trouver le fabuleux trésor qu\'il disait avoir laissé. A 17 ans, Luffy prend la mer dans une petite barque avec pour but de réunir un équipage de pirates, mais de pirates pas comme les autres, qui devront partager sa conception un peu étrange de la piraterie. L\'aventure est lancée.', '1997-12-24', '2020-04-03', 'Shueisha', 97, 974, 22), ('나 혼자만 레벨업', 'Solo Leveling, Only I level up, Na Honjaman Level Up', 'Shonen', 'Sung Jin Woo est considéré comme le plus faible des Chasseurs de rang E... Autrement dit le plus faible parmi les faibles. Il est tellement faible qu\'il est surnommé par ses confrères, le « Faible ». Avec une équipe de Chasseurs, il se rend dans un donjon de rang D. Malheureusement, l\'équipe se retrouve piégée dans une salle avec des monstres qui ne sont pas du tout du niveau du donjon... S\'en suit un massacre... Et Sung Jin Woo, aux portes de la mort arrive à acquérir une capacité pour le moins étrange...', '2019-09-26', '2020-08-27', 'D&C Media, Kakao', 3, 126, 23);")
 """user = User(username = "admin", password = "password")
 session.add(user)
 
