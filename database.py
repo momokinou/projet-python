63866d3a26019659d3f05456dbe9b058f4f4ee89
qmoriceau <quentin.moriceau@ipilyon.net>
2020-11-15 17:04:18 +0100

    ajout 404

--- database.py
+++ database.py
@@ -31,7 +31,7 @@ def __init__(self):
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    nickname = Column(Text)
+    username = Column(Text)
     password = Column(Text)
     email = Column(Text)
     gender = Column(Enum('male', 'female', 'unknown'))
@@ -96,10 +96,10 @@ Base.metadata.create_all(engine)
 """Session = sessionmaker(bind=engine)
 session = Session()"""
 
-engine.execute("INSERT INTO users(nickname, password, email, gender, id_language) VALUES ('vani', 'supervani', 'vani@gmail.fr', 'male', 14), ('hamtoé', 'jambon94', 'hamtoé@mail.com', 'male', 14), ('superuser', 'jesuissuperuser', 'user@user.fr', 'unknown', 28), ('moi', 'lui', 'elle@onnesaitplus.net', 'unknown', 5), ('Jacqueline', 'monchienestblanc', 'jacquelinevillard@hotmail.fr', 'female', 13);")
+engine.execute("INSERT INTO users(username, password, email, gender, id_language) VALUES ('vani', 'supervani', 'vani@gmail.fr', 'male', 14), ('hamtoé', 'jambon94', 'hamtoé@mail.com', 'male', 14), ('superuser', 'jesuissuperuser', 'user@user.fr', 'unknown', 28), ('moi', 'lui', 'elle@onnesaitplus.net', 'unknown', 5), ('Jacqueline', 'monchienestblanc', 'jacquelinevillard@hotmail.fr', 'female', 13);")
 engine.execute("INSERT INTO language(name) VALUES ('Arabic'), ('Bengali'), ('Bulgarian'), ('Burmese'), ('Catalan'), ('Chinese(Simp)'), ('Chinese(Hong-Kong)'), ('Czech'), ('Danish'), ('Dutch'), ('English'), ('Filipino'), ('Finnish'), ('French'), ('German'), ('Greek'), ('Hebrew'), ('Hindi'), ('Hungarian'), ('Indonesian'), ('Italian'), ('Japanese'), ('Korean'), ('Lithuanian'), ('Malay'), ('Mongolian'), ('Norwegian'), ('Other'), ('Persian'), ('Polish'), ('Portuguese(BR)'), ('Portuguese(PT)'), ('Romanian'), ('Russian'), ('Serbo-Russian'), ('Spanish(ES)'), ('Spanish(LATAM)'), ('Swedish'), ('Thai'), ('Turkish'), ('Ukrainian'), ('Vietnamese');")
 engine.execute("INSERT INTO manga(title, alt_title, category, description, release_date, ending_date, studio, nbr_volume, nbr_chap) VALUES ('ワンピース', 'One Piece', 'Shonen', \"Gloire, fortune et puissance, c'est ce que possédait Gold Roger, le tout puissant roi des pirates, avant de mourir sur l'échafaud. Mais ses dernières paroles ont éveillées bien des convoitises, et lança la fabuleuse ère de la piraterie, chacun voulant trouver le fabuleux trésor qu\'il disait avoir laissé. A 17 ans, Luffy prend la mer dans une petite barque avec pour but de réunir un équipage de pirates, mais de pirates pas comme les autres, qui devront partager sa conception un peu étrange de la piraterie. L'aventure est lancée.\", '1997-12-24', '2020-04-03', 'Shueisha', 97, 974), ('나 혼자만 레벨업', 'Solo Leveling, Only I level up, Na Honjaman Level Up', 'Shonen', \"Sung Jin Woo est considéré comme le plus faible des Chasseurs de rang E... Autrement dit le plus faible parmi les faibles. Il est tellement faible qu'il est surnommé par ses confrères, le « Faible ». Avec une équipe de Chasseurs, il se rend dans un donjon de rang D. Malheureusement, l\'équipe se retrouve piégée dans une salle avec des monstres qui ne sont pas du tout du niveau du donjon... S\'en suit un massacre... Et Sung Jin Woo, aux portes de la mort arrive à acquérir une capacité pour le moins étrange...\", '2019-09-26', '2020-08-27', 'D&C Media, Kakao', 3, 126);")
-engine.execute("INSERT INTO chapter (id, nbr_chapter, title, id_manga, id_language) VALUES(1, 1, 'Romance Dawn', 4, 11),(2, 1, 'They call him Straw Hat Luffy', 4, 11),(3, 1, '', 5, 11),(4, 1, '', 5, 14);")
+engine.execute("INSERT INTO chapter (nbr_chapter, title, id_manga, id_language) VALUES( 1, 'Romance Dawn', 4, 11),( 1, 'They call him Straw Hat Luffy', 4, 11),(1, '', 5, 11),(1, '', 5, 14);")
 """user = User(username = "admin", password = "password")
 session.add(user)
 
