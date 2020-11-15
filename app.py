3f623601842afebe5c01e3f84e6cff14bc1f4ac7
Florine Michel <florinemichel@orange.fr>
2020-11-15 18:07:15 +0100

    correction search path

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from database import *
 engine = create_engine('mysql+pymysql://root:@localhost/StreamingSite')
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.secret_key = os.urandom(12)
 
 
@@ -25,7 +26,7 @@ def do_admin_login():
     
     Session = sessionmaker(bind=engine)
     s = Session()
-    query = s.query(User).filter(User.nickname.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
     result = query.first()
     if result:
         session['logged_in'] = True
@@ -37,19 +38,36 @@ def do_admin_login():
 @app.route("/search", methods=['POST'])
 def search():
     POST_TITLE = str(request.form['search'])
-    query = engine.execute("SELECT title FROM manga WHERE title LIKE %\"manga\"%")
+    session = sessionmaker(bind=engine)
+    s = session()
+    result = s.query(Manga).filter(or_(Manga.title.like("%%" + POST_TITLE + "%%"), Manga.alt_title.like("%%" + POST_TITLE + "%%")))
+
+    for row in result:
+        print(row.title)
+        return logout()
+    else:
+        return render_template('404.html', manga=POST_TITLE)
 
 
 @app.route("/home", methods=['POST'])
 def manga():
     POST_TITLE = str(request.form['manga'])
-    query = engine.execute("SELECT title FROM manga ORDER BY asc")
+    session = sessionmaker(bind=engine)
+    s = session()
+    result = s.query(Manga).filter(Manga.title)
+    for row in result:
+        print(row.title)
 
 
 @app.route("/manga", methods=['POST'])
 def read():
     POST_PAGE = str(request.form['manga'])
     query = engine.execute("SELECT title FROM chapter c, INNER JOIN manga m ON c.id_manga = m.id")
+    session = sessionmaker(bind=engine)
+    s = session()
+    result = s.query(Chapter).filter(Manga.title.like('%ンピ%'), Manga.title.like('%ンピ%'))
+    for row in result:
+        print(row.title)
 
 
 @app.route("/logout")
